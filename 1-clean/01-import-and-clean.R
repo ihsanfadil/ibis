1bb29f8b59b65448dc733212bc25b9e7ad63fdd1
ihsanfadil <ihsn.fdlh@gmail.com>
2021-09-01 13:31:50 +0700

    uploaded raw data.

---
+++ 1-clean/01-import-and-clean.R
@@ -0,0 +1,49 @@
+
+# Preamble ----------------------------------------------------------------
+
+# Code author  : Ihsan Fadilah
+# Email        : ifadilah@eocru.org
+# Last updated : September 2021
+# Project      : Indonesia Brain Infection Study
+
+# This script provides reproducible code for merging, standardising, and
+# cleaning the data independently collected from two sites of the study, namely
+# Jakarta and Bandung. The raw datasets are expected to be left untouched after
+# downloading from the clinical databases to make sure code below and afterwards
+# remain reproducible throughout the project,i.e., when new participants are
+# added to each of the database.
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)  # Tidy code
+library(readxl)     # Read Excel files
+library(labelled)   # Label variables
+
+# Import ------------------------------------------------------------------
+
+# Read the data in
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Appendix ----------------------------------------------------------------
+
+
